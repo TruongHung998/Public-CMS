b8ede831ab11251e5b163f2ce18a1c03b2a3acf6
Mac <truonghung239810@gmail.com>
2025-05-07 22:27:19 +0700

    _: refactor

--- my-project/src/api/ios/controllers/ios.ts
+++
@@ -1,7 +0,0 @@
-/**
- * ios controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::ios.ios');
--- my-project/src/api/ios/routes/ios.ts
+++
@@ -1,7 +0,0 @@
-/**
- * ios router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::ios.ios');
